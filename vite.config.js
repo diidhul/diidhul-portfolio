b33d840268e99e6f251944b62504dabc991ad9a6
diidhul <fadhil.elsafer@gmail.com>
2025-01-30 11:16:12 +0700

    deploy config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// import ghPages from "vite-plugin-gh-pages";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), ghPages()],
-  base: "/diidhul-portfolio/",
+  plugins: [react()],
+  base: "/diidhul-portfolio/", // Gantilah dengan nama repository GitHub Anda
 });
